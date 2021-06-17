e884d18b1859c938222b8204b9f033a14ca8631b
Sherwin Variancia <sherwinvariancia@gmail.com>
2021-06-17 13:02:18 +0700

    refactor

--- xlsinject.py
+++ xlsinject.py
@@ -1,28 +1,162 @@
 import random
+import numpy
 import xlsxwriter
 import names
 import calendar
+import time
+import json
+import os
 from datetime import datetime, timedelta
+from selenium import webdriver
 
 def main():
-    totalData = 100
-    workbook = xlsxwriter.Workbook("GFormBotData.xlsx")
-    worksheet = workbook.add_worksheet("Sheet1")
-    rowIndex = 8
-    for row in range(totalData):
-        day,month,year = raw_dob_generator()
-        userGender = random.choice(["male","female"])
+    #read JSON data
+    json_file = open("data.json")
+    json_array = json.load(json_file)
 
-        worksheet.write('A'+str(rowIndex), name_generator(names.get_first_name(gender=userGender), userGender))
-        worksheet.write('B'+str(rowIndex), dob_generator(day,month,year))
-        worksheet.write('C'+str(rowIndex), age_generator(year))
-        worksheet.write('D'+str(rowIndex), nik_generator(day + month + str(year)))
-        worksheet.write('E'+str(rowIndex), phone_generator())
-        worksheet.write('F'+str(rowIndex), status_generator(userGender))
-        worksheet.write('G'+str(rowIndex), checkbox_generator())
+    #set global variables
+    global male_names, female_names,occupations,non_male_occupations
+    male_names = json_array['malenames']
+    female_names = json_array['femalenames']
+    occupations = json_array['occupations']
+    non_male_occupations = json_array['non_male_occupations']
+
+    while (True):
+        clearConsole()
+        print( 
+            "=========================================" + "\n"
+            "       Fake User Details Generator" + "\n"
+            "=========================================" + "\n"
+            "(1) Generate User Details" + "\n"
+            "(2) Generate User Details for Excel File" + "\n"
+            "(3) Direct Inject to Form (Require Code Editing)" + "\n"
+            "(4) Exit" + "\n"
+        )
+        user_option = int(input("Select option: "))
+
+        if(user_option==1):
+            generate_locally()
+        elif(user_option==2):
+            generate_excel()
+        elif(user_option==3):
+            inject_directly()
+        elif(user_option==4):
+            break;
+
+
+def generate_locally():
+    totalData = input("Select number of data to be generated: ")
+    for x in range(int(totalData)):
+        name, dob, age, nik, phonenumber, occupation, option = generate_data()
+        print(
+            "==================================" + "\n"
+            "Injected Data Count :" + str(int(x)+1) + "\n"
+            "==================================" +"\n"
+            "NIK       : " + str(nik) + "\n"
+            "Nama      : " + name + "\n"
+            "DOB       : " + dob + "\n"
+            "Umur      : " + str(age) + "\n"
+            "No HP     : " + str(phonenumber) + "\n"
+            "Pekerjaan : " + occupation + "\n"
+        )
+    input("Press Enter to continue...")
+
+def generate_excel():
+    totalData = input("Select number of data to be generated: ")
+    fileName = input("Enter file name (include .xlsx extension): ")
+    sheetName = input("Enter sheet name: ")
+    startRow = input("Enter starting row: ")
+    workbook = xlsxwriter.Workbook(fileName)
+    worksheet = workbook.add_worksheet(sheetName)
+    rowIndex = int(startRow)
+    for row in range(int(totalData)):
+        name, dob, age, nik, phonenumber, occupation, option = generate_data()
+        worksheet.write('A'+str(rowIndex), name)
+        worksheet.write('B'+str(rowIndex), dob)
+        worksheet.write('C'+str(rowIndex), age)
+        worksheet.write('D'+str(rowIndex), nik)
+        worksheet.write('E'+str(rowIndex), phonenumber)
+        worksheet.write('F'+str(rowIndex), occupation)
+        worksheet.write('G'+str(rowIndex), option)
         rowIndex += 1
-
     workbook.close()
+
+    input("Data Generated. Press Enter to continue...")
+
+def inject_directly():
+    totalData = input("Select number of data to be injected: ")
+    googleFormLink = "https://docs.google.com/forms/d/e/1FAIpQLScajZqdye35dJpi0abhiuyX51C50XLAo6ZahOQTN37_zX5cjg/viewform"
+    for x in range (int(totalData)):
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
+        options  = webdriver.ChromeOptions()
+        options.headless = True
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-running-insecure-content')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--proxy-server='direct://'")
+        options.add_argument("--proxy-bypass-list=*")
+        options.add_argument("--start-maximized")
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+        options.add_argument("-incognito")
+        browser = webdriver.Chrome(executable_path="D:/Coding/Python/chromedriver.exe", options=options)
+        browser.get(googleFormLink)
+        time.sleep(2) 
+        name, dob, age, nik, phonenumber, occupation, option = generate_data()
+        nama = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        nama.send_keys(name)
+        tanggalLahir = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        tanggalLahir.send_keys(dob)
+        umur = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        umur.send_keys(age)
+        nik = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        nik.send_keys(nik)
+        nomorHp = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[5]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        nomorHp.send_keys(phonenumber)
+        toko = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[2]/div[6]/div/div/div[2]/div/div[1]/div[2]/textarea")
+        toko.send_keys(occupation)
+        checkboxes = browser.find_elements_by_class_name("appsMaterialWizTogglePapercheckboxCheckbox")       
+        time.sleep(1) 
+        if(option == 1):
+            checkboxes[0].click()
+        else:
+            checkboxes[1].click()
+        submitButton = browser.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div/div")
+        time.sleep(1)
+        submitButton.click()
+
+        browser.close()
+
+        print(
+            "==================================" + "\n"
+            "Injected Data Count :" + str(int(user_option)+1) + "\n"
+            "==================================" +"\n"
+            "NIK       : " + str(nik) + "\n"
+            "Nama      : " + name + "\n"
+            "DOB       : " + dob + "\n"
+            "Umur      : " + str(age) + "\n"
+            "No HP     : " + str(phonenumber) + "\n"
+            "Pekerjaan : " + occupation + "\n"
+        )
+    input("Data Injected. Press Enter to continue...")
+
+def generate_data():
+    day,month,year = raw_dob_generator()
+    userGender = random.choice(["male","female"])
+
+    #generate all other details
+    generatedName = name_generator(names.get_first_name(gender=userGender), userGender) 
+    generatedDob = dob_generator(day,month,year)
+    generatedAge = age_generator(year)
+    generatedNik = nik_generator(day + month + str(year))
+    generatedPhoneNumber = phone_generator()
+    generatedOcuppation = occupation_generator(userGender)
+    generatedOption = checkbox_generator()
+    
+    return generatedName, generatedDob, generatedAge, generatedNik, generatedPhoneNumber, generatedOcuppation, generatedOption
 
 def raw_dob_generator():
     months = ["01","02","03","04","05","06","07","08","09","10","11","12"]
@@ -45,56 +179,52 @@ def phone_generator():
     return front+str(randomEndDigit)
 
 def checkbox_generator():
-    chocies = ["1","2","1,2"]
-    return random.choice(chocies)
+    return random.choice(["1","2"])
 
 def age_generator(year):
     currentYear = datetime.now().year
     return currentYear-year
 
 def dob_generator(day,month,year):
-    choice = random.choice([1,2,3,4])
+    choice = random.choice([1,2,3])
 
     if (choice == 1):
         return day +"/"+ month +"/"+ str(year)
     elif (choice == 2):
         return day +"-"+ month +"-"+ str(year)
     elif (choice == 3):
-        return day +" "+ month +" "+ str(year)
-    elif (choice == 4):
         month = calendar.month_name[int(month)]
         return day +" "+ month +" "+ str(year)
 
-def status_generator(gender):
-    status = ""
+def occupation_generator(gender):
+    list_of_occupations = occupations.copy()
     if (gender == "male"):
-        status = random.choice(["Porter", "Bengkel", "Montir", "Kerja","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Wiraswasta","Ilmuwan","Satpam", "Nelayan", "Sopir", "Perawat","Teknisi","Jualan","Kerja di pasar","Porter", "Bengkel", "Montir", "Kerja","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Wiraswasta","Ilmuwan","Satpam", "Nelayan", "Sopir", "Perawat","Teknisi","Jualan","Kerja di pasar","Porter", "Bengkel", "Montir", "Kerja","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Wiraswasta","Ilmuwan","Satpam", "Nelayan", "Sopir", "Perawat","Teknisi","Jualan","Kerja di pasar", "Farmasi","Arsitek","Akuntan","Respsionis","Toko", "Bidan","Pengacara", "Programmer", "Peternak", "Tukang", "Jual koran", "Pramusaji","Penulis", "Dokter", "Pramusaji", "Pramusaji", "Pramusaji" ,"Anak Mafia", "Presiden"])
-    else:
-        status = random.choice(["Ibu Rumah Tangga","Asisten Rumah Tangga", "Pramusaji", "Pramusaji", "Peternak", "Tukang", "Porter", "Bengkel", "Montir", "Kerja", "Dokter","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Ibu Rumah Tangga","Asisten Rumah Tangga", "Peternak", "Tukang", "Porter", "Bengkel", "Montir", "Kerja", "Dokter","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Ibu Rumah Tangga","Asisten Rumah Tangga", "Peternak", "Tukang", "Porter", "Bengkel", "Montir", "Kerja", "Dokter","Asisten Rumah Tangga","Kuli","ART","Kuliah","Pensiun","Pengangguran","Guru", "Tidak bekerja", "Sales","IT","Petani","Wiraswasta","Ilmuwan","Satpam", "Nelayan", "Sopir", "Perawat","Teknisi","Jualan","Kerja di pasar","Farmasi","Arsitek","Akuntan","Respsionis","Toko", "Bidan","Pengacara", "Programmer", "Penulis", "Jual koran", "Pramusaji","Anak Mafia", "Presiden"])
-
-    stringFormat = length = random.choice([0,0,0,0,0,1,1])
-    if (stringFormat == 1):
-        status = status.lower()
-
-    return status
+        for item in non_male_occupations:
+            list_of_occupations.remove(item)
+    return format_string(random.choice(list_of_occupations))
 
 
 def name_generator(name, gender):
-    malesName = ["Abichandra","Abimana","Abimanyu","Abinaya","Abyasa","Adhi","Adika","Adinata","Adipramana","Aditya","Adiwangsa","Agam","Agnibrata","Agung","Aji","Andaru","Andi","Angkasa","Apta","Aradhana","Ardhi","Ardiyanto","Arga","Arief","Arjanta","Arya","Asmaralaya","Astaguna","Aswangga","Bagas","Bagaskoro","Bajradaka","Bakti","Bamantara","Bambang","Banyu","Baskoro","Basuki","Baswara","Batara","Bayu","Bhagawanta","Bhanu","Bimo","Birawa","Bisma","Brahma","Bramantya","Brawijaya","Buana","Budi","Budiono","Cakra","Cakrawala","Candra","Cipta","Damar","Daniswara","Danurdara","Darsa","Dewangga","Dharma","Dipa","Dimas","Dirga","Edi","Endaru","Estu","Endang","Fadh","Fadhly","Fajar","Gadhing","Ganendra","Gardara","Gentala","Galih","Ganesh","Gibran","Gilang","Guinandra","Gumelar","Gunawan","Guntur","Guritno","Gusti","Hadi","Hakim","Halim","Hamdan","Hanafi","Handaru","Hapsari","Hardana","Haribawa","Haris","Harjita","Hartadi","Hartanto","Haryanto","Herdian","Herjuno","Hendro","Henry","Hery","Ihsan","Ihsan","Ismoyono","Jamal","Janu","Jati","Jatmika","Jaya","Jenaka","Jenggala","Jiwa","Joko","Jumanta","Jumantara","Kala","Kamajaya","Kamandaka","Karisma","Karno","Karunia","Kawindra","Kresna","Lakeswara","Langit","Lasmana","Leksana","Lesmana","Luthfi","Malik","Manggala","Mardhi","Margi","Mulyadi","Naresh","Nareswara","Naufal","Nugraha","Nusantara","Panca","Perdana","Perkasa","Praba","Pradigta","Pradipto","Pradnyana","Pradnyana","Pramuditha","Pramudya","Pranadipa","Pranawa","Prasaja","Prasetyo","Putra","Raden","Raditya","Rahardian","Raharja","Madyana","Madya","Mahadri","Mahajana","Mahapraja","Mahaprana","Mahardika","Mahatma","Mahendra","Raka","Rama","Randika","Rangga","Reswara","Reza","Rezvan","Ricky","Rudi","Sadana","Sakti","Samudra","Santoso","Satria","Satriya","Satya","Sigit","Siswanto","Sobiyanto","Sudiro","Sugiarto","Surya","Suryana","Susilo","Tamawijaya","Tegar","Teja","Tirta","Tohpati","Tulus","Ulung","Wardana","Wajendra","Wibisana","Wibowo","Widayaka","Widura","Widodo","Wijaya","Wirya","Yoga","Yuda","Yudayana"]
-
-    femalesName = ["Adiratna","Adriani","Airani","Amanda","Anatari","Anindya","Anjani","April","Arkadewi","Aruna","Arsyana","Asri","Ayu","Ayudisha","Bandiani","Banurasmi","Basagita","Batari","Binar","Bintang","Bratarini","Bulan","Cahaya","Candramaya","Cempaka","Citra","Citta","Cyntia","Danastri","Danurdara","Dewi","Dhatu","Diajeng","Dianti","Diatmika","Ditya","Dwi","Elok","Erina","Estiana","Fitri","Gadis","Gahyaka","Gantari","Garini","Gayatri","Gema","Gemani","Gemintang","Gempita","Genta","Ginanita","Gyandra","Halim","Haira","Hanasta","Hanna","Harini","Hasana","Hasya","Hayu","Ika","Ina","Indah","Indira","Indri","Indriaya","Intan","Istari","Isthika","Isty","Iswari","Jayanti","Jelita","Jenar","Juwita","Kahyang","Kamala","Kana","Kani","Kanista","Karina","Karmika","Kartana","Kasidya","Kasih","Kasyaira","Kayshila","Keinan","Kemala","Kencana","Keswari","Kila","Kinnas","Kirana","Laksita","Laksmi","Lalita","Lanita","Laras","Larasati","Lasmaya","Lawana","Laya","Lelana","Lestari","Lestia","Lika","Limar","Lily","Lingga","Listia","Listu","Lituhayu","Lukita","Mada","Madaharsa","Madana","Madarsana","Mahadewi","Maharani","Mahawirya","Maktika","Malya","Manda","Manika","Mataya","Maya","Mega","Melati","Mentari","Mirah","Miratussany","Mustika","Nada","Nadia","Nadindra","Naeswari","Nala","Nanda","Nara","Nararya","Narasnama","Nata","Ndari","Nehan","Nidya","Ningrum","Ningsih","Nirmala","Numatya","Padmana","Paramastri","Paramita","Parmadita","Pawana","Permata","Pertiwi","Pramata","Pramidita","Pratista","Puri","Purnama","Puspa","Puspita","Putri","Radmila","Rahmi","Rajini","Rananta","Rani","Ranupatma","Rara","Ratih","Ratimaya","Ratna","Ratu","Rawika","Rembulan","Rosa","Sada","Saktika","Sakya","Samada","Saraswati","Sasi","Sasmaya","Sekar","Septha","Dwi","Seta","Sitaresmi","Sukma","Swasti","Tantri","Tanya","Tarasari","Tisna","Tiwi","Tri","Tyas","Utami","Utari","Wangi","Wasana","Waskita","Wening","Widati","Widhiani","Widi","Widia","Widuri","Wikasita","Wiyana","Wulan","Wulandari","Wuri","Yanti","Yatiwara","Yatna","Yetri","Yulianti","Yuni"]
-    length = random.choice([0,0,1,1,1,1,1,1,1,1,1,1,2,2,2,3])
+    length = numpy.random.choice([0,1,2,3], p=[0.2,0.5,0.25,0.05])
     name = name + " "
     for x in range(length):
         if(gender=="male"):
-            name += random.choice(malesName) +" "
+            name += random.choice(male_names) +" "
         else:
-            name += random.choice(femalesName) +" "
+            name += random.choice(female_names) +" "
+    return format_string(name);
 
-    stringFormat = length = random.choice([0,0,0,0,0,1,1])
+def format_string(text):
+    stringFormat = numpy.random.choice([0,1], p=[0.15,0.85])
     if (stringFormat == 1):
-        name = name.lower()
+        return text.lower()
+    else:
+        return text
 
-    return name;
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    
 
 main()
